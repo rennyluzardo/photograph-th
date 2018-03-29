0effffc8b00c722748d3969d8853c7d00d42cb18
Renny Luzardo <rennyjoseluzardocolina@gmail.com>
2018-03-29 09:11:07 -0500

    Ingracion API 1.1 de twitter
    Creado servicio y metodo para obtener tweets

--- src/app/noticias.service.ts
+++ src/app/noticias.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { resolve, reject } from 'q';
 // import { HttpClient } from '@angular/common/http';
 // import { HttpHeaders } from '@angular/common/http';
 
@@ -9,28 +11,25 @@ const dataConfig = {
   "consumerSecret": "ZDw1SQ2lgsRu6loTR7DvSARUAEmYUeUEiz8CgYNUEZRiAOAUzZ",
   "accessToken": "283903787-GrfcCsBH72pWyDA1YwWx0taKWm3BSEehTNfcUPAk",
   "accessTokenSecret": "5iNUeZpK2MaDMqlKUs4vVc3bGzDrDFyojca3zJLTYtPUz",
-  "callBackUrl": "http://localhost:4200/"
+  "callBackUrl": ""
 }
 var twitter = new Twitter(dataConfig);
 
-var error = function (err, response, body) {
-  console.log('ERROR [%s]', err);
-}
-var success = function (data) {
-  console.log('Data [%s]', data);
-}
 @Injectable()
 export class NoticiasService {
-  constructor() {
-    console.log(twitter.getUserTimeline({ screen_name: '_luzardorenny' }, error, success))
+  constructor() {}
+
+  getTwts = ()=> {
+    let twtsTotales = []
+    twitter.getUserTimeline({ screen_name: '_luzardorenny' }, error => error, twts => {
+      let twtsParsed = JSON.parse(twts)
+      for (let twt in twtsParsed) {
+        twtsTotales.push(twtsParsed[parseInt(twt)])
+        if (twtsTotales.length > 4 ) {
+          break
+        }
+      }
+    })
+    return twtsTotales
   }
-
-  // getBearer = () => {
-  //   let configReq = {
-
-  //   }
-  //   this.http.post('https://api.twitter.com/oauth2/token', configReq).subscribe()
-  // }
-
-
 }
--- src/app/noticias/noticias.component.ts
+++ src/app/noticias/noticias.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { NoticiasService } from '../noticias.service'
+
+var Twitter = require('twitter-node-client').Twitter;
+
+const dataConfig = {
+  "consumerKey": "bZVyVqbW1TCN3oNNAwuZE12Uc",
+  "consumerSecret": "ZDw1SQ2lgsRu6loTR7DvSARUAEmYUeUEiz8CgYNUEZRiAOAUzZ",
+  "accessToken": "283903787-GrfcCsBH72pWyDA1YwWx0taKWm3BSEehTNfcUPAk",
+  "accessTokenSecret": "5iNUeZpK2MaDMqlKUs4vVc3bGzDrDFyojca3zJLTYtPUz",
+  "callBackUrl": ""
+}
+var twitter = new Twitter(dataConfig)
+
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
   styleUrls: ['./noticias.component.css'],
   providers: [NoticiasService]
 })
+
 export class NoticiasComponent implements OnInit {
-
-  constructor( private noticias:NoticiasService ) { }
-
+  constructor( private noticias: NoticiasService) { 
+    this.noticias.getTwts()
+  }  
+  totaltwts = this.noticias.getTwts()
   ngOnInit() {
   }
-
 }
