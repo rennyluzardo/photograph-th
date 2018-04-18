ecd1fe031fb80336c55bef2c2f8b70fe26bfd479
Renny Luzardo <rennyjoseluzardocolina@gmail.com>
2018-04-17 21:08:19 -0500

    tweets list

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,16 @@
 import { Request, Response, Router } from "express"
-import * as twitter from 'twitter-node-client'
+
+const Twitter = require('twitter-node-client').Twitter
+
+const twtConf = {
+    "consumerKey": "bZVyVqbW1TCN3oNNAwuZE12Uc",
+    "consumerSecret": "ZDw1SQ2lgsRu6loTR7DvSARUAEmYUeUEiz8CgYNUEZRiAOAUzZ",
+    "accessToken": "283903787-GrfcCsBH72pWyDA1YwWx0taKWm3BSEehTNfcUPAk",
+    "accessTokenSecret": "5iNUeZpK2MaDMqlKUs4vVc3bGzDrDFyojca3zJLTYtPUz",
+    "callBackUrl": ""
+}
+
+const twitter = new Twitter(twtConf)
 const path = require("path")
 
 const router : Router = Router()
@@ -8,7 +19,7 @@ router.get('/', (req: Request, res:Response)=>{
     res.sendFile(path.join('/index.html'))
 })
 router.get('/tweets_list', (req: Request, res:Response)=>{
-    res.send('Lista de Tweets')
+    twitter.getUserTimeline({ screen_name: '_luzardorenny'}, error=>res.send(error), success =>res.send(success))
 })
 
 export const routes = router
